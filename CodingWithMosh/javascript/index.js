b9481b4950af14f94558e60a0928315821ca96c7
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-05-14 17:02:38 -0400

    completed beginning of course

--- CodingWithMosh/javascript/index.js
+++ CodingWithMosh/javascript/index.js
@@ -1,7 +1,14 @@
 // the inputs when defining a function are called "parameters"
+// Performing a task
 function greet(name, lastName) { // the parameters here are 'name' and 'lastName"
     console.log('Hello ' + name + ' ' + lastName);
 }
 
+// Calculate a Value
+function square(number) {
+    return number * number; 
+}
+
+
 // Things you input into a function when its called are called "arguments"
-greet('John', 'Smith'); // the arguments here are 'John" and 'Smith'
+console.log(square(2)); // the arguments here are 'square()" and '2'
