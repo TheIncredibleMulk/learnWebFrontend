e0a9a13a3b9bcf716fd42191efd2bfa8002c8c03
TheIncredibleMulk <andrew.mulkey@gmail.com>
2021-06-14 00:41:13 -0400

    added some news from static data file

--- freecodecamp_JAMstack/.eleventy.js
+++ freecodecamp_JAMstack/.eleventy.js
@@ -1,13 +1,10 @@
-module.exports = function(config) {
-
-
-    config.addPassthroughCopy("src/js");
-    return {
-        dir: {
-            input: "src",
-            output: "dist"
-        }
-    };
-    
+module.exports = function (config) {
+  config.addPassthroughCopy("src/js");
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data",
+    },
+  };
 };
-
